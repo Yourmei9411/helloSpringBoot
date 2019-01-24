745a0f6ae3b1dbaa3ec16316139a30d4c6bdc315
Yourmei <405140692@qq.com>
2019-01-24 22:35:28 +0800

    spring starter project + web
    错误: 找不到或无法加载主类 caffrey.HellospringBootApplication

---
+++ HellospringBoot/src/main/java/caffrey/HellospringBootApplication.java
@@ -0,0 +1,14 @@
+package caffrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringBootApplication.class, args);
+	}
+
+}
+
---
+++ HellospringBoot/src/main/java/caffrey/ServletInitializer.java
@@ -0,0 +1,14 @@
+package caffrey;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HellospringBootApplication.class);
+	}
+
+}
+
